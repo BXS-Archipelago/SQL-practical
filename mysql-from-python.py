32a5e9b6cad917bc7e0e679982d22447e225f67b
BXS-Archipelago <bxs@tutanota.com>
2021-05-05 18:59:19 +0000

    insert new table and row

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 import pymysql
 
 # get username from could9 workspace
@@ -15,12 +16,12 @@ connection = pymysql.connect(host='localhost',
 
 try:
     # Run a Query
-    with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+    with connection.cursor() as cursor:   
+        row = ("Bob", 21, "1990-02-06 23:04:56")    
+        cursor.execute(" INSERT INTO Friends VALUES(%s, %s, %s);", row)
+        connection.commit()
+
 finally:
-    #close the connection regardless of whether the above
-    #was successful or not
+    # close the connection regardless of whether the above
+    # was successful or not
     connection.close()
